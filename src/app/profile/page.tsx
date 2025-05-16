eb26abbaf97423cf1e5cdff4d7faf869c972277f
Muhammad Firzak Rizki <firzakrizki@gmail.com>
2025-05-16 21:32:49 +0700

    Create About and Profile Pages

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,8 @@
+export default function Profile() {
+    return (
+        <div>
+            <h1>User Profile</h1>
+            <p>This is the profile page of the user.</p>
+        </div>
+    );
+}
