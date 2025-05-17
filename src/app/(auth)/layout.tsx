2d494ea5efb9948bc976809b4c3ff77d8eb2dbc6
Muhammad Firzak Rizki <firzakrizki@gmail.com>
2025-05-17 23:33:47 +0700

    Multiple Layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,11 @@
-import './globals.css'
+import '@/app/globals.css'
 
 export const metadata = {
     title: 'My Next.js App',
     description: 'This is a simple Next.js application.',
 }
 
-export default function RootLayout({
+export default function AuthLayout({
     children,
 }: {
     children: React.ReactNode
@@ -14,12 +14,9 @@ export default function RootLayout({
         <html lang="en">
 
             <body>
-                <header className="bg-blue-600 text-white p-4">
-                    <h1 className="text-2xl font-bold text-center">Header</h1>
-                </header>
                 {children}
                 <footer className="bg-blue-600 text-white p-4">
-                    <p className="text-center">Footer</p>
+                    <p className="text-center">Footer Auth</p>
                 </footer>
             </body>
         </html>
---
+++ src/app/(marketing)/layout.tsx
@@ -0,0 +1,27 @@
+import '@/app/globals.css'
+
+export const metadata = {
+    title: 'My Next.js App',
+    description: 'This is a simple Next.js application.',
+}
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+
+            <body>
+                <header className="bg-blue-600 text-white p-4">
+                    <h1 className="text-2xl font-bold text-center">Header</h1>
+                </header>
+                {children}
+                <footer className="bg-blue-600 text-white p-4">
+                    <p className="text-center">Footer</p>
+                </footer>
+            </body>
+        </html>
+    )
+}
