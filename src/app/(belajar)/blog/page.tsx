02f4850ba6570e4b33ada8ea6b5e59b42efcdea5
Muhammad Firzak Rizki <firzakrizki@gmail.com>
2025-05-21 19:35:32 +0700

    Loading Component

--- src/app/(belajar)/blog/page.tsx
+++ src/app/(belajar)/blog/page.tsx
@@ -1,14 +1,18 @@
 export const metadata = {
-    title: {
-        absolute: 'Blog',
-    },
-}
+  title: {
+    absolute: 'Blog',
+  },
+};
 
-export default function Blog() {    
-    return (
-        <div>
-            <h1>Blog</h1>
-            <p>This is the blog page of our Next.js application.</p>
-        </div>
-    );
+// Fungsi server component
+export default async function Blog() {
+
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  return (
+    <div>
+      <h1>Blog</h1>
+      <p>This is the blog page of our Next.js application.</p>
+    </div>
+  );
 }
